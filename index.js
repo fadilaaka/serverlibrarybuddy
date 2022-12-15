43449320294cc3cba782f4e499d5431abff467c4
fadilaaka <akafadila09@gmail.com>
2022-12-15 12:16:05 +0700

    trust proxyu 1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connect(
 app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set("trust proxy");
+app.set("trust proxy", 1);
 app.use(express.json());
 
 app.use(
